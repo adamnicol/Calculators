2df3f12a613184f1f1f9edea39f12217aab42c21
Adam Nicol <adam.nicol@protonmail.com>
2025-09-24 21:28:52 +0100

    Add react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { queryClient } from "./lib/react-query.ts";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from "./routes.ts";
 import { RouterProvider } from "react-router";
 import { StrictMode } from "react";
@@ -9,6 +12,9 @@ const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
