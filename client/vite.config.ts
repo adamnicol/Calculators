3aa8cc7c3f0accde91fe93825d0c4762e7b5a50f
Adam Nicol <adam.nicol@protonmail.com>
2025-09-26 13:58:41 +0100

    Add docker compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Allows connecting to Docker on localhost.
+    host: "0.0.0.0",
+    watch: {
+      // Required for hot reloading in Docker
+      usePolling: true,
+    },
+  },
+});
